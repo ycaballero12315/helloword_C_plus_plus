37797f8a42456f8021e54d86768d3c1594322e8b
Yoeny Caballero Gonzalez <ycaballero12315@gmail.com>
2025-11-07 09:49:25 -0300

    constantes

---
+++ constantes.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    const double pi = 3.1495;
+    double radio = 10;
+    double circunf = 2*radio*pi;
+    std::cout<<"Circunferencia:" <<circunf << " cm"<<"\n";
+    return 0;
+}
